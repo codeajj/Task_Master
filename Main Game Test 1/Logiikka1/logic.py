51cc5e2f6959236811fe2f12fc0e8045b1d3d64f
codeajj <paavobro@gmail.com>
2025-05-06 15:41:10 +0300

    Toimii, taso 1 ja 2

--- Main Game Test 1/Logiikka1/logic.py
+++ Main Game Test 1/Logiikka1/logic.py
@@ -6,7 +6,7 @@ class GameLogic:
         self.current_task = None
         self.task_index = { #Indexi on kyseisen maa
             0: [self.task_0_0, self.task_0_1, self.task_0_2, self.task_0_3, self.task_0_4],
-            #1: [self.task_1_0, self.task_1_1, self.task_1_2, self.task_0_3, self.task_1_4]
+            1: [self.task_1_0, self.task_1_1, self.task_1_2, self.task_1_3, self.task_1_4]
         }
     def process_input(self, user_input): #Otetaan input, tarkastetaan mitä kirjoitettu.
         user_input = user_input.lower().strip()
@@ -90,6 +90,46 @@ class GameLogic:
     def task_0_4(self, answer):
         if answer == "question":
             return "Task 5: You bump into Mesi, do you take a picture with him?", False
+        elif answer == "yes":
+            return "Correct!", True
+        else:
+            return "Incorrect!", False
+
+    def task_1_0(self, answer):
+        if answer == "question":
+            return "Task 1: What ominous rock lies in the center of Australia?", False
+        elif answer == "uluru":
+            return "Correct!", True
+        else:
+            return "Incorrect!", False
+
+    def task_1_1(self, answer):
+        if answer == "question":
+            return "What is the most populous city in Australia?", False
+        elif answer == "sydney":
+            return "Correct!", True
+        else:
+            return "Incorrect!", False
+
+    def task_1_2(self, answer):
+        if answer == "question":
+            return f"Task 3: How big is Australia? a) 9 million km2 b) 7.7 million km2 or c) 2 million km2", False
+        elif answer == "b":
+            return "Correct! I think?", True
+        else:
+            return "Incorrect!", False
+
+    def task_1_3(self, answer):
+        if answer == "question":
+            return "Task 4: A kangaroo insulted your mother, punch him?", False
+        elif answer == "yes":
+            return "Correct!", True
+        else:
+            return "Incorrect!", False
+
+    def task_1_4(self, answer):
+        if answer == "question":
+            return "Task 5: Go surfing?", False
         elif answer == "yes":
             return "Correct!", True
         else:
